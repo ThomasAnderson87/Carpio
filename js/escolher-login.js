163e8229df35701f1a511d0a4927294c5d3d6424
Gustavo lucena silva <gustavolucena12@gmail.com>
2021-11-07 18:07:13 -0300

    resolvendo responsividades, bugs e adicionando javaScript na tela de login

---
+++ js/escolher-login.js
@@ -0,0 +1,23 @@
+const login = document.querySelector('.faz-login')
+const cadastro = document.querySelector('.faz-cadastro')
+
+const formLogin = document.querySelector('.login-box')
+const formCadastro = document.querySelector('.cadastro-box')
+
+login.addEventListener('click', ()=> {
+   
+   cadastro.classList.remove('login-escolhido')
+   formCadastro.classList.add('display-none')
+
+   login.classList.add('login-escolhido')
+   formLogin.classList.remove('display-none')
+})
+
+cadastro.addEventListener('click', ()=> {
+
+   login.classList.remove('login-escolhido')
+   formLogin.classList.add('display-none')
+
+   cadastro.classList.add('login-escolhido')
+   formCadastro.classList.remove('display-none')
+})
